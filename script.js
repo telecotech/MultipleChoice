1831038915589c9f577f15d125a921cf10f63395
Mullens <telecotech82@gmail.com>
2023-02-18 08:40:37 -0500

    criteria changes

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let timeLeft = 60;
-let currentQuestion = 1;
+let currentQuestion = 0;
+let quizStarted = false;
 
 const questions = [
   {
@@ -19,7 +20,26 @@ const questions = [
   }
 ];
 
+function startQuiz() {
+  quizStarted = true;
+  displayQuestion();
+  let timer = setInterval(function() {
+    timeLeft--;
+    document.getElementById("timer").innerHTML = timeLeft + " seconds remaining";
+    if (timeLeft <= 0) {
+      clearInterval(timer);
+      document.getElementById("timer").innerHTML = "Time's up!";
+      document.querySelectorAll("button").forEach(button => button.disabled = true);
+      document.getElementById("question-container").innerHTML = "";
+      timeLeft = 60;
+      currentQuestion = 0;
+      quizStarted = false;
+    }
+  }, 1000);
+}
+
 function displayQuestion() {
+  currentQuestion++;
   const questionContainer = document.getElementById("question-container");
   const question = questions[currentQuestion-1];
   questionContainer.innerHTML = `
@@ -40,21 +60,16 @@ function checkAnswer(correctAnswer) {
   } else {
     alert("Incorrect!");
   }
-  currentQuestion++;
-  if (currentQuestion <= questions.length) {
-    displayQuestion();
-  } else {
+  if (currentQuestion >= questions.length) {
     clearInterval(timer);
     document.getElementById("timer").innerHTML = "Quiz completed!";
+    document.querySelectorAll("button").forEach(button => button.disabled = true);
+    document.getElementById("question-container").innerHTML = "";
+    document.getElementById("timer").innerHTML = "";
+    timeLeft = 60;
+    currentQuestion = 0;
+    quizStarted = false;
+  } else {
+    displayQuestion();
   }
 }
-
-let timer = setInterval(function() {
-  timeLeft--;
-  document.getElementById("timer").innerHTML = timeLeft + " seconds remaining";
-  if (timeLeft <= 0) {
-    clearInterval(timer);
-    document.getElementById("timer").innerHTML = "Time's up!";
-    document.querySelectorAll("button").forEach(button => button.disabled = true);
-  }
-}, 1000);
